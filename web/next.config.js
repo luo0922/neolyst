f21dc4f5c28a218f82c71f082a26a38bdc398e6c
luo0922 <522855715>
2026-04-01 16:05:38 +0800

    报告推送neoport文件大小限制

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',  // 必须在这里，不能在任何 experimental 里面
-  // 其他配置...
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
 };
 
 module.exports = nextConfig;
